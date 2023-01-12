7115241adb2b6517c0cd013af91a5982dc2a1303
Explore8488 <e038317.bakre@dlc.ui.edu.ng>
2023-01-12 19:44:46 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(){
+    // + for addition
+    // - for sub
+    // * multiplication
+    // / for divisi
+    // % for mod
+    int x = 0;
+
+    x = x + 1;
+    x *= 2;
+    x = x * 2;
+    int y = 5;
+    y = y + 1;
+    y += 1;
+    y++;
+
+    y--;
+
+    // comparison operation
+
+    // less than <
+    // greater than >
+    // equalls to ==
+    // less than or equall to <=
+    // greter than or equalls to >=
+    // not equall to !=
+
+    //loop
+
+    //for loop
+
+    // print hello wrld 10 times
+    int i;
+
+    for (i = 0; i < 5; ++i){
+
+        printf("The current value of i is: %d\n", i);
+
+    }
+    //while loop to print i am a programmer 6 times
+
+    int j = 0;
+    do{
+        printf("%d I am a programmer \n", j);
+        j++;
+    }while (j < 6);
+
+
+
+
+    //while loop
+    //do while
+
+    y = x;
+    y == x;
+
+ 
+
+}
